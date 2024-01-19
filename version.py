fa54001fb70f4d3f13f37ec5c885c822725bacd6
krishna khadka <krishnakhadka2802@gmail.com>
2024-01-20 01:15:45 +0530

    Adding poetry as a build tool

--- version.py
+++
@@ -1,9 +0,0 @@
-def _safe_int(string):
-    try:
-        return int(string)
-    except ValueError:
-        return string
-
-
-__version__ = "0.0.5"
-VERSION = tuple(_safe_int(x) for x in __version__.split("."))
